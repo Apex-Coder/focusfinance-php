efa917c13680af751621b9c670eadf512fe76bfc
Apex-Coder <micnic123@gmail.com>
2021-07-04 09:09:18 -0400

    Initial Setup - Code optimization and cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -72,6 +73,24 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+    // .addPlugin(new CopyWebpackPlugin(
+    //     [
+    //         { from: './assets/images', to: 'images' },
+    //         { options: { concurrency: 100 } }
+    //     ],
+
+    // ))
+
+    // .addPlugin(new CopyWebpackPlugin([
+    //     { from: './assets/images', to: 'images' }   
+    // ]))
+
+    .addPlugin(new CopyWebpackPlugin({
+        patterns: [
+            { from: './assets/images', to: 'images' }
+        ]
+    }))
+    
 ;
 
 module.exports = Encore.getWebpackConfig();
